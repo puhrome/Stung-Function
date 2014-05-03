8559036bc55dc6c926bf4457dfd2da380d562dc4
puhrome <phirom.yim@fullsail.edu>
2014-05-02 21:30:53 -0400

    Stung_Function
    Using function to calculate how many bee stings it takes to kill an
    animal by weight

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Stung Created by Phirom Yim on 5/2/14.
+ */
+
+var total = weight (8.66666667, ("Enter victim's weight"));
+function weight(stings, pounds){
+    var beeStings = 8.66666667 * (prompt("Enter victim's weight"));
+    return beeStings;
+}
+console.log(total);
